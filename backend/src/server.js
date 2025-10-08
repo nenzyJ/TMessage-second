d8b0cb51620bcbfbe7e48813a042ebbd1be54e08
nenzyJ <jamside0807@gmail.com>
2025-10-08 12:07:12 +0300

    db fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,8 +4,6 @@ import { connectDB } from "./config/db.js";
 
 const app = express();
 
-connectDB();
-
 app.get("/", (req, res) => res.send("Hello from server"));
 const startServer = async () => {
   try {
