b57d291c0201d8c9c3724d31bd44e6194dce3b85
nenzyJ <jamside0807@gmail.com>
2025-10-11 18:27:56 +0300

    comment & notification routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,9 @@ import cors from "cors"
 import {clerkMiddleware} from "@clerk/express"
 import userRoutes from "./routes/user.route.js"
 import postRoutes from "./routes/post.route.js"
+import commentRoutes from "./routes/comment.route.js"
+import notificationRoutes from "./routes/notification.route.js"
+
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 
@@ -18,6 +21,8 @@ app.get("/", (req, res) => res.send("Hello from server"));
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/notifications", notificationRoutes)
 
 //error handling middleware
 app.use((err, req, res, next) => {
