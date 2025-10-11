8552d81359a482f9dda181f2382f1cb432c1fbb4
nenzyJ <jamside0807@gmail.com>
2025-10-11 18:42:11 +0300

    arcjet added

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,7 @@ import notificationRoutes from "./routes/notification.route.js"
 
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
 
 
 const app = express();
@@ -16,6 +17,7 @@ app.use(cors());
 app.use(express.json())
 
 app.use(clerkMiddleware());
+app.use(arcjetMiddleware);
 
 app.get("/", (req, res) => res.send("Hello from server"));
 
